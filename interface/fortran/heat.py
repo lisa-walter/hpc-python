ab1ff9c3457f3acf6370857d3bb52a5075c7730e
Lisa Walter <lisawalter4@googlemail.com>
2020-05-20 15:58:04 +0200

    finished week 3

--- interface/fortran/heat.py
+++ interface/fortran/heat.py
@@ -2,27 +2,10 @@ import numpy as np
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
+from fortran_module import evolve
 
 # Set the colormap
 plt.rcParams['image.cmap'] = 'BrBG'
-
-def evolve(u, u_previous, a, dt, dx2, dy2):
-    """Explicit time evolution.
-       u:            new temperature field
-       u_previous:   previous field
-       a:            diffusion constant
-       dt:           time step. """
-
-    n, m = u.shape
-
-    for i in range(1, n-1):
-        for j in range(1, m-1):
-            u[i, j] = u_previous[i, j] + a * dt * ( \
-             (u_previous[i+1, j] - 2*u_previous[i, j] + \
-              u_previous[i-1, j]) / dx2 + \
-             (u_previous[i, j+1] - 2*u_previous[i, j] + \
-                 u_previous[i, j-1]) / dy2 )
-    u_previous[:] = u[:]
 
 def iterate(field, field0, a, dx, dy, timesteps, image_interval):
     """Run fixed number of time steps of heat equation"""
@@ -34,6 +17,9 @@ def iterate(field, field0, a, dx, dy, timesteps, image_interval):
     # for the size of the time-step:
     dt = dx2*dy2 / ( 2*a*(dx2+dy2) )    
 
+    field = np.asfortranarray(field)
+    field0 = np.asfortranarray(field0)
+
     for m in range(1, timesteps+1):
         evolve(field, field0, a, dt, dx2, dy2)
         if m % image_interval == 0:
@@ -42,7 +28,7 @@ def iterate(field, field0, a, dx, dy, timesteps, image_interval):
 def init_fields(filename):
     # Read the initial temperature field from file
     field = np.loadtxt(filename)
-    field0 = field.copy() # Array for field of previous time step
+    field0 = np.copy(field) # Array for field of previous time step
     return field, field0
 
 def write_field(field, step):
